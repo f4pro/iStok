6509c3bb9c227fda18180c21f271b7b053c8f35c
irgiyogap01 <irgi19ti@mahasiswa.pcr.ac.id>
2021-06-18 21:27:05 +0700

    TAMBAHAN FORM

---
+++ application/views/user/vw_user.php
@@ -0,0 +1,37 @@
+<div class="col-md-6 my-4">
+  <h1 class="page-title"><?php echo $judul; ?></h1>
+  <div class="col-md-6"><a href="<?= base_url() ?>barang/tambah" class="btn btn-info mb-4">Tambah Barang</a></div>
+  <div class="card shadow">
+    <div class="card-body">
+    
+      <h5 class="card-title"> Tabel Barang</h5>
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Nama Barang</th>
+            <th>Jenis Barang</th>
+            <th>Stok Barang</th>
+            <th>Gambar</th>
+            <th>Aksi</th>
+          </tr>
+        </thead>
+        <tbody>
+        
+          <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>
+                  <a href=" " class="badge badge-pill badge-warning">Hapus</a>
+                  <a href=" "class="badge badge-pill badge-danger">Edit</a>
+          </tr>
+         
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+</div>
