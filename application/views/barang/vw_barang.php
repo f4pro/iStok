8d3b0a28069f228aa7cbe47a4c28212587b2c8ec
f4pro <faizilputra7@gmail.com>
2021-06-18 23:10:57 +0700

    Update vw_barang: Table

--- application/views/barang/vw_barang.php
+++ application/views/barang/vw_barang.php
@@ -1,37 +1,40 @@
-<div class="col-md-6 my-4">
-  <h1 class="page-title"><?php echo $judul; ?></h1>
-  <a href="<?= base_url() ?>barang/tambah" type="button" class="btn btn-primary mb-4" data-toggle="modal" data-target="#eventModal"><span class="fe fe-plus fe-16 mr-3"></span>Barang</a>
-  <div class="card shadow">
-    <div class="card-body">
-    
-      <h5 class="card-title"> Tabel Barang</h5>
-      <table class="table table-hover">
-        <thead>
-          <tr>
-            <th>No</th>
-            <th>Nama Barang</th>
-            <th>Jenis Barang</th>
-            <th>Stok Barang</th>
-            <th>Gambar</th>
-            <th>Aksi</th>
-          </tr>
-        </thead>
-        <tbody>
-        
-          <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td>
+<div class="card-body">
+  <div id="dataTable-1_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer">
+    <div class="col-sm-12">
+      <h1 class="page-title"><?php echo $judul; ?></h1>
+      <a href="<?= base_url() ?>barang/tambah" type="button" class="btn btn-primary mb-4" data-toggle="modal" data-target="#eventModal"><span class="fe fe-plus fe-16 mr-3"></span>Barang</a>
+      <div class="card shadow">
+        <div class="card-body">
+
+          <h5 class="card-title"> Tabel Barang</h5>
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Nama Barang</th>
+                <th>Jenis Barang</th>
+                <th>Stok Barang</th>
+                <th>Gambar</th>
+                <th>Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
                   <a href=" " class="badge badge-pill badge-warning">Hapus</a>
-                  <a href=" "class="badge badge-pill badge-danger">Edit</a>
-          </tr>
-         
-        </tbody>
-      </table>
+                  <a href=" " class="badge badge-pill badge-danger">Edit</a>
+              </tr>
+
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   </div>
-</div>
 </div>
