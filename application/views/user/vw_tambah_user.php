2565e6d773bce16734c512576ec19a999e14fd7b
irgiyogap01 <irgi19ti@mahasiswa.pcr.ac.id>
2021-07-06 22:51:18 +0700

    baru

--- application/views/user/vw_tambah_user.php
+++ application/views/user/vw_tambah_user.php
@@ -14,24 +14,24 @@
                                 <div class="form-row">
                                     <div class="form-group col-md-6">
                                         <label for="inputEmail4">Nama</label>
-                                        <input type="text" class="form-control" id="nama_karyawan" placeholder="">
+                                        <input type="text" name='nama' value="<?= set_value('nama');?>"  class="form-control" id="nama" placeholder="Nama User...">
+                                        <?= form_error('nama', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
                                     <div class="form-group col-md-6">
-                                        <label for="inputPassword4">Status</label>
-                                        <select class="form-control" id="example-select">
-                                            <option>User</option>
-                                            <option>Admin</option>
-                                            <option>Validasi</option>
-                                        </select>
+                                        <label for="inputPassword4">Email</label>
+                                        <input type="text" name='email' value="<?= set_value('email');?>"  class="form-control" id="email" placeholder="Email...">
+                                        <?= form_error('nama_barang', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="inputAddress">email</label>
-                                    <input type="text" class="form-control" id="alamat" placeholder="">
                                 </div>
                                 <div class="form-group">
                                     <label for="inputAddress">Password</label>
-                                    <input type="password" class="form-control" id="notelp" placeholder="">
+                                    <input type="text" name='password' value="<?= set_value('nama_barang');?>"  class="form-control" id="nama_barang" placeholder="Nama Barang...">
+                                    <?= form_error('nama_barang', '<small class="text-danger pl-3">', '</small>'); ?>
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputAddress">Status</label>
+                                    <input type="text" name='nama_barang' value="<?= set_value('nama_barang');?>"  class="form-control" id="nama_barang" placeholder="Nama Barang...">
+                                    <?= form_error('nama_barang', '<small class="text-danger pl-3">', '</small>'); ?>
                                 </div>
                                 <button type="submit" class="btn btn-primary">Submit</button>
                             </form>
