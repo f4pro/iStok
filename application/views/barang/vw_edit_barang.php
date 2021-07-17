bf3fe5dccc23bac137d226c7063254c1a2b2fdac
f4pro <faizilputra7@gmail.com>
2021-07-17 20:54:00 +0700

    Edit barang

--- application/views/barang/vw_edit_barang.php
+++ application/views/barang/vw_edit_barang.php
@@ -11,34 +11,35 @@
             </div>
             <div class="card-body">
               <form action="" method="POST" enctype="multipart/form-data">
-              <input type="hidden" name="id" value="<?= $barang['id']; ?>">
+                <input type="hidden" name="id" value="<?= $barang['id']; ?>">
                 <div class="form-row">
                   <div class="form-group col-md-6">
                     <label for="inputEmail4">Nama Barang</label>
-                    <input type="text" name='nama_barang' value="<?= $barang['nama_barang'];?>"  class="form-control" id="nama_barang" placeholder="Nama Barang...">
+                    <input type="text" name='nama_barang' value="<?= $barang['nama_barang']; ?>" class="form-control" id="nama_barang" placeholder="Nama Barang...">
                     <?= form_error('nama_barang', '<small class="text-danger pl-3">', '</small>'); ?>
                   </div>
                   <div class="form-group col-md-6">
                     <label for="inputPassword4">Jenis Barang</label>
-                    <input type="text" name='jenis_barang' value="<?= $barang['jenis_barang'];?>"  class="form-control" id="jenis_barang" placeholder="Jenis Barang...">
+                    <input type="text" name='jenis_barang' value="<?= $barang['jenis_barang']; ?>" class="form-control" id="jenis_barang" placeholder="Jenis Barang...">
                     <?= form_error('jenis_barang', '<small class="text-danger pl-3">', '</small>'); ?>
                   </div>
-                <div class="form-group">
-                  <label for="inputAddress">Stok</label>
-                  <input type="text" name='stok' value="<?= $barang['stok'];?>"  class="form-control" id="stok" placeholder="Stok Barang...">
+                  <div class="form-group col-md-6">
+                    <label for="inputAddress">Stok</label>
+                    <input type="text" name='stok' value="<?= $barang['stok']; ?>" class="form-control" id="stok" placeholder="Stok Barang...">
                     <?= form_error('stok', '<small class="text-danger pl-3">', '</small>'); ?>
-                </div>
+                  </div>
 
-                <div class="form-group">
+                  <div class="form-group col-md-6">
                     <img src="<?= base_url('assets/barang/') . $barang['gambar']; ?>" style="width: 100px" class="img-thumbnail">
-                    <div class="custom-file">
-
+                    <div class="custom-file col-md-6">
                       <input type="file" class="custom-file-input" name="gambar" id="gambar">
                       <label for="gambar" class="custom-file-label">Choose File</label>
-                </div>
-              
-                <a href="<? base_url('Barang') ?>" class="btn btn-danger">Kembali</a>
-                <button type="submit" name="Tambah" class="btn btn-primary">Tambah</button>
+                    </div>
+                  </div>
+                  <div class="form-group col-md-6">
+                    <a href="<? base_url('Barang') ?>" class="btn btn-danger">Kembali</a>
+                    <button type="submit" name="Tambah" class="btn btn-primary">Tambah</button>
+                  </div>
               </form>
             </div> <!-- /. card-body -->
           </div> <!-- /. card -->
