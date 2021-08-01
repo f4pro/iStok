b28ccb80f3615184cda2d58b00a2276f357488a8
f4pro <faizilputra7@gmail.com>
2021-08-01 20:26:50 +0700

    GG

--- application/views/lapor/vw_edit_support.php
+++ application/views/lapor/vw_edit_support.php
@@ -16,11 +16,11 @@
                   <div class="form-group col-md-4">
                     <label for="nama">Rate</label>
                     <?php if ($support['rate'] == "URGENT") { ?>
-                      <button class="btn btn-danger form-control">URGENT</button>
+                      <button type="button" class="btn btn-danger form-control">URGENT</button>
                     <?php } elseif ($support['rate'] == "MEDIUM") { ?>
-                      <button class="btn btn-warning form-control">MEDIUM</button>
+                      <button type="button" class="btn btn-warning form-control">MEDIUM</button>
                     <?php } else { ?>
-                      <button class="btn btn-info form-control">LOW</button>
+                      <button type="button" class="btn btn-info form-control">LOW</button>
                     <?php } ?>
                   </div>
                   <div class="form-group col-md-4">
@@ -40,38 +40,37 @@
                   </div>
                   <div class="form-group col-md-4">
                     <label for="karyawan">Nama Karyawan</label>
-                    <select name="karyawan" id="karyawan" class="form-control">
-                                <?php foreach ($karyawan as $p) : ?>
-                                    <option value="<?= $p['id']; ?>" <?php if ($support['karyawan'] == $p['id']) {
-                                                                            echo "selected";
-                                                                        } ?>> <?= $p['nama_karyawan']; ?></option>
-                                <?php endforeach; ?>
-                            </select>
-
+                    <select name="karyawan" id="karyawan" class="custom-select form-control" disabled>
+                      <?php foreach ($karyawan as $p) : ?>
+                        <option value="<?= $p['id']; ?>" <?php if ($support['karyawan'] == $p['id']) {
+                                                            echo "selected";
+                                                          } ?>> <?= $p['nama_karyawan']; ?></option>
+                      <?php endforeach; ?>
+                    </select>
                   </div>
                 </div>
                 <div class="form-group">
                   <label for="email">Judul Laporan</label>
-                  <input type="text" name='jenis_support' value="<?= $support['jenis_support']; ?>" class="form-control" id="jenis_support">
+                  <input type="text" name='jenis_support' value="<?= $support['jenis_support']; ?>" class="form-control" id="jenis_support" disabled>
                 </div>
                 <div class="form-group">
                   <label for="email">Descripsi</label>
-                  <textarea class="form-control" id="example-textarea" rows="4" disabled></textarea>
+                  <textarea class="form-control" id="example-textarea" rows="4" disabled><?= $support['description']?></textarea>
                 </div>
                 <hr class="my-4">
                 <div class="form-group">
                   <label for="email">Reply</label>
-                  <textarea class="form-control" id="example-textarea" rows="4"></textarea>
+                  <textarea class="form-control" name="description_feed" id="example-textarea" rows="4"><?= $support['description_feed']?></textarea>
                 </div>
                 <!-- <div class="form-group">
                   <a href="<?= base_url('Account/') ?>" class="btn btn-secondary">Kembali</a>
                 </div> -->
                 <div class="form-row">
                   <div class="form-group col-md-6">
-                    <a href="<?= base_url('Account/') ?>" class="btn btn-secondary form-control">Kembali</a>
+                    <a href="<?= base_url('Support/') ?>" class="btn btn-secondary form-control">Kembali</a>
                   </div>
                   <div class="form-group col-md-6">
-                    <button type="submit" name="edit" class="btn btn-primary form-control">Konfimasi Perubahan</button>
+                    <button type="submit" name="Edit" class="btn btn-primary form-control">Konfimasi Perubahan</button>
                   </div>
                 </div>
               </form>
