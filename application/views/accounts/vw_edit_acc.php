bdbe47b989e6b29291e397bc003efd67ca176d58
f4pro <faizilputra7@gmail.com>
2021-08-01 13:56:41 +0700

    AA

--- application/views/accounts/vw_edit_acc.php
+++ application/views/accounts/vw_edit_acc.php
@@ -14,23 +14,39 @@
                 <input type="hidden" name="id" value="<?= $user['id']; ?>">
                 <div class="form-row">
                   <div class="form-group col-md-6">
-                    <label for="inputEmail4">Nama User</label>
-                    <input type="text" name='nama_barang' value="<?= $user['nama']; ?>" class="form-control" id="nama_barang" placeholder="Nama Barang..." disabled>
+                    <label for="nama">Nama User</label>
+                    <input type="text" name='nama' value="<?= $user['nama']; ?>" class="form-control" id="nama_barang" disabled>
                     <!-- <?= form_error('nama_barang', '<small class="text-danger pl-3">', '</small>'); ?> -->
                   </div>
                   <div class="form-group col-md-6">
-                    <label for="inputPassword4">Password</label>
+                    <label for="email">Email</label>
+                    <input type="text" name='email' value="<?= $user['email']; ?>" class="form-control" id="email">
+                    <!-- <?= form_error('nama_barang', '<small class="text-danger pl-3">', '</small>'); ?> -->
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="password">Password</label>
                     <input type="password" name='password' value="<?= $user['password']; ?>" class="form-control" id="password" disabled>
                     <!-- <?= form_error('jenis_barang', '<small class="text-danger pl-3">', '</small>'); ?> -->
                   </div>
                   <div class="form-group col-md-6">
                     <label for="status">Status</label>
-                    <input type="text" name='status' value="<?= $user['status']; ?>" class="form-control" id="status">
+                    <select type="text" name="status" class="form-control" id="status">
+                      <?php if ($user['status'] == "Admin") { ?>
+                        <option selected>Admin [Current]</option>
+                      <?php } elseif ($user['status'] == "Karyawan") { ?>
+                        <option selected>Karyawan [Current]</option>
+                      <?php } else { ?>
+                        <option selected>Proses [Current]</option>
+                      <?php } ?>
+                      <option value="Admin">Administrator</option>
+                      <option value="Karyawan">Karyawan</option>
+                      <option value="Proses">Validasi</option>
+                    </select>
                     <!-- <?= form_error('stok', '<small class="text-danger pl-3">', '</small>'); ?> -->
                   </div>
                   <div class="form-group col-md-6">
                     <a href="<?= base_url('Account/') ?>" class="btn btn-secondary">Kembali</a>
-                    <button type="submit" name="Tambah" class="btn btn-warning">Konfimasi Perubahan</button>
+                    <button type="submit" name="edit" class="btn btn-warning">Konfimasi Perubahan</button>
                   </div>
               </form>
             </div> <!-- /. card-body -->
